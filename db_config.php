f2da2d41332db434a0035a49a9e457c61fa936f8
Brayan Lujan Munoz <brayan234.lm@hotmail.com>
2021-10-03 02:19:19 -0500

    Actualización de los archivos php

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER', "gxdgxpkmgzdund"); // Usuario
-define('DB_PASSWORD', "8dff6b021aa12a83c9c4074e5bd6ef32eaae3c7e253914cfee80924a86b3f72d"); // Password
-define('DB_DATABASE', "d4706j93dti0tl"); // Nombre de la base de datos
-define('DB_SERVER', "ec2-52-207-47-210.compute-1.amazonaws.com"); // host server
+define('DB_USER', "jxrzezmcbxsrin"); // Usuario
+define('DB_PASSWORD', "187fc84163f43bb321309d63db4a4f29cc65b830d104c8b7d64d361513cdfa48"); // Password
+define('DB_DATABASE', "d9u91c7t1qqgls"); // Nombre de la base de datos
+define('DB_SERVER', "ec2-34-233-105-94.compute-1.amazonaws.com"); // host server
 ?>
