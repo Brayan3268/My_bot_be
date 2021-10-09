bf24b80a8ef1a612284c6f78b74c5ae93585c588
Brayan Lujan Munoz <brayan234.lm@hotmail.com>
2021-10-09 16:12:53 -0500

    Add the page for the data base for read things to do finished

---
+++ things_to_do_SELECT.php
@@ -0,0 +1,33 @@
+<?php
+	require 'SQLGlobal.php';
+
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		try{
+			//$datos = json_decode(file_get_contents("php://input"),true);
+
+			$respuesta = SQLGlobal::selectArray("select * from thingstodo");
+            if($respuesta){
+                echo json_encode(array(
+                    'respuesta'=>'200',
+                    'estado' => 'Se obtuvierón los datos correctamente',
+                    'data'=>$respuesta,
+                    'error'=>''
+                ));
+            }else{
+                echo json_encode(array(
+                    'respuesta'=>'100',
+                    'estado' => 'No existen estos datos',
+                    'error'=>''
+                ));
+            }
+		}catch(PDOException $e){
+			echo json_encode(
+				array(
+					'respuesta'=>'-1',
+					'estado' => 'Ocurrio un error, intentelo mas tarde',
+					'data'=>'',
+					'error'=>$e->getMessage())
+			);
+		}
+	}
+?>
