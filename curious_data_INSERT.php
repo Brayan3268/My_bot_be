4dacb0cc321a03f1ae9bd84986ed99d8545f123a
Brayan Lujan Munoz <brayan234.lm@hotmail.com>
2022-02-01 17:00:12 -0500

    add pages for insert and select curiousData v1

---
+++ curious_data_INSERT.php
@@ -0,0 +1,40 @@
+<?php
+	require 'SQLGlobal.php';
+
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		try{
+			$datos = json_decode(file_get_contents("php://input"),true);
+
+			$curiousdata = $datos["curiousData"]; // obtener parametros POST
+
+            $respuesta = SQLGlobal::cudFiltro(
+				"insert into curiousData(curiousData) values (?);",
+				array($curiousdata)
+			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+            if($respuesta > 0){
+                echo json_encode(array(
+                    'respuesta'=>'200',
+                    'estado' => 'Se agrego correctamente la sugerencia',
+                    'data'=> 'El numero de registros afectados son: '.$respuesta,
+                    'error'=>''
+                ));
+            }else{
+                echo json_encode(array(
+                    'respuesta'=>'100',
+                    'estado' => 'No se agrego correctamente la sugerencia',
+                    'data'=> 'El numero de registros afectados son: '.$respuesta,
+                    'error'=>''
+                ));
+            }
+
+		}catch(PDOException $e){
+			echo json_encode(
+				array(
+					'respuesta'=>'-1',
+					'estado' => 'Ocurrio un error, intentelo mas tarde',
+					'data'=>'',
+					'error'=>$e->getMessage())
+			);
+		}
+	}
+?>
