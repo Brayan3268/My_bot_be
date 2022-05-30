4b6986d1c3dacfcbbee57555dbd19ac92c69851e
Brayan L <brayanlujan.mzu@hotmail.com>
2022-05-30 12:35:13 -0500

    shortJokes insert and read pages added

---
+++ shortJoke_INSERT.php
@@ -0,0 +1,40 @@
+<?php
+	require 'SQLGlobal.php';
+
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		try{
+			$datos = json_decode(file_get_contents("php://input"),true);
+
+			$shortJoke = $datos["shortJoke"]; // obtener parametros POST
+
+            $respuesta = SQLGlobal::cudFiltro(
+				"insert into shortJoke(shortJoke) values (?);",
+				array($shortJoke)
+			);//con filtro ("El tamaño del array debe ser igual a la cantidad de los '?'")
+            if($respuesta > 0){
+                echo json_encode(array(
+                    'respuesta'=>'200',
+                    'estado' => 'Se agrego correctamente el chiste corto',
+                    'data'=> 'El numero de registros afectados son: '.$respuesta,
+                    'error'=>''
+                ));
+            }else{
+                echo json_encode(array(
+                    'respuesta'=>'100',
+                    'estado' => 'No se agrego correctamente el chiste corto',
+                    'data'=> 'El numero de registros afectados son: '.$respuesta,
+                    'error'=>''
+                ));
+            }
+
+		}catch(PDOException $e){
+			echo json_encode(
+				array(
+					'respuesta'=>'-1',
+					'estado' => 'Ocurrio un error, intentelo mas tarde',
+					'data'=>'',
+					'error'=>$e->getMessage())
+			);
+		}
+	}
+?>
