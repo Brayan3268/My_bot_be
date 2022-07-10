60eb9629adee33edd8577fe853cb8163aacfe7bc
Brayan L <brayanlujan.mzu@hotmail.com>
2022-07-10 17:54:54 -0500

    page movieorseries change 1 insert and select fixed (only put series for the moment)

---
+++ serie_SELECT.php
@@ -0,0 +1,34 @@
+<?php
+	require 'SQLGlobal.php';
+
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+		try{
+			//$datos = json_decode(file_get_contents("php://input"),true);
+
+			$respuesta = SQLGlobal::selectArray("select * from serie");
+            if($respuesta){
+                echo json_encode(array(
+                    'respuesta'=>'200',
+                    'estado' => 'Se obtuvieron los datos correctamente',
+                    'data'=>$respuesta,
+                    'error'=>''
+                ));
+            }else{
+                echo json_encode(array(
+                    'respuesta'=>'100',
+                    'estado' => 'No existen datos sobre las películas o las series',
+                    'error'=>''
+                ));
+            }
+		}catch(PDOException $e){
+			echo json_encode(
+				array(
+					'respuesta'=>'-1',
+					'estado' => 'Ocurrio un error, intentelo mas tarde',
+					'data'=>'',
+					'error'=>$e->getMessage())
+			);
+		}
+	}
+
+?>
