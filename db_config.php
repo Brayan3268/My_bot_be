d07b235d9e2d349fb83d1eadbc879dff0cf30e62
Brayan L <brayanlujan.mzu@hotmail.com>
2023-05-11 00:03:52 -0500

    second commit

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER', "jxrzezmcbxsrin"); // Usuario
-define('DB_PASSWORD', "187fc84163f43bb321309d63db4a4f29cc65b830d104c8b7d64d361513cdfa48"); // Password
-define('DB_DATABASE', "d9u91c7t1qqgls"); // Nombre de la base de datos
-define('DB_SERVER', "ec2-34-233-105-94.compute-1.amazonaws.com"); // host server
+define('DB_USER', "doadmin"); // Usuario
+define('DB_PASSWORD', "AVNS_3wxL3GfZSNK4P9y3q4A"); // Password
+define('DB_DATABASE', "defaultdb"); // Nombre de la base de datos
+define('DB_SERVER', "db-postgresql-nyc3-31867-do-user-14084220-0.b.db.ondigitalocean.com"); // host server
 ?>
