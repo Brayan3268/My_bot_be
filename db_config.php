baa82878c107138c825e6b54ad946716dfa3d2b9
Brayan L <brayanlujan.mzu@hotmail.com>
2023-05-11 11:59:28 -0500

    Port added

--- db_config.php
+++ db_config.php
@@ -2,5 +2,5 @@
 define('DB_USER', "doadmin"); // Usuario
 define('DB_PASSWORD', "AVNS_3wxL3GfZSNK4P9y3q4A"); // Password
 define('DB_DATABASE', "defaultdb"); // Nombre de la base de datos
-define('DB_SERVER', "db-postgresql-nyc3-31867-do-user-14084220-0.b.db.ondigitalocean.com"); // host server
+define('DB_SERVER', "db-postgresql-nyc3-31867-do-user-14084220-0.b.db.ondigitalocean.com:25060"); // host server
 ?>
