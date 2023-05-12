a6126b44d48cdb3c98bae14ac360fed65f3446f2
Brayan L <brayanlujan.mzu@hotmail.com>
2023-05-11 20:29:59 -0500

    Changes for show

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 
 <?php
-define('DB_USER', ""); // Usuario
+define('DB_USER', "doadmin"); // Usuario
 define('DB_PASSWORD', "AVNS_3wxL3GfZSNK4P9y3q4A"); // Password
 define('DB_DATABASE', "defaultdb"); // Nombre de la base de datos
 define('DB_SERVER', "db-postgresql-nyc3-31867-do-user-14084220-0.b.db.ondigitalocean.com"); // host server
