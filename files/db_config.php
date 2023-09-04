fa4e0a28dc1466c76078aba76fc88825313ed7cf
Brayan <brayanlujan.mzu@hotmail.com>
2023-09-04 12:57:31 -0500

    Settings for noone

--- files/db_config.php
+++ files/db_config.php
@@ -1,8 +1,8 @@
 
 <?php
-define('DB_USER', "_38a6d2c8c52dc998"); // Usuario
-define('DB_PASSWORD', "_3f89e68921c6fa1c8bbe47ba01a47f"); // Password
-define('DB_DATABASE', "_e8e523c607cb"); // Nombre de la base de datos
-define('DB_SERVER', "primary.postgresql--xgvtvgx4zlx9.addon.code.run"); // host server
-define('DB_PORT', "5432"); // Número de puerto
+define('DB_USER', ""); // Usuario
+define('DB_PASSWORD', ""); // Password
+define('DB_DATABASE', ""); // Nombre de la base de datos
+define('DB_SERVER', ""); // host server
+define('DB_PORT', ""); // Número de puerto
 ?>
