2ff43c9b81f432b3800e00ee7b97b361ae0c1a5e
Brayan <brayanlujan.mzu@hotmail.com>
2023-09-04 12:40:46 -0500

    Settings for northflank

--- files/db_config.php
+++ files/db_config.php
@@ -1,8 +1,8 @@
 
 <?php
-define('DB_USER', "doadmin"); // Usuario
-define('DB_PASSWORD', "AVNS_3wxL3GfZSNK4P9y3q4A"); // Password
-define('DB_DATABASE', "defaultdb"); // Nombre de la base de datos
-define('DB_SERVER', "db-postgresql-nyc3-31867-do-user-14084220-0.b.db.ondigitalocean.com"); // host server
-define('DB_PORT', "25060"); // Número de puerto
+define('DB_USER', "_38a6d2c8c52dc998"); // Usuario
+define('DB_PASSWORD', "_3f89e68921c6fa1c8bbe47ba01a47f"); // Password
+define('DB_DATABASE', "_e8e523c607cb"); // Nombre de la base de datos
+define('DB_SERVER', "primary.postgresql--xgvtvgx4zlx9.addon.code.run"); // host server
+define('DB_PORT', "5432"); // Número de puerto
 ?>
